96e30d601255e4b55acdea01fed9defb0a6b04fa
delkafriyo <delkafriyo@gmail.com>
2026-07-21 13:35:47 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-import getDate from "./src/date_convertion.js";
-import readline from "node:readline/promises";
-import { stdin as input, stdout as output } from "node:process";
-
-// import { createInterface } from "node:readline";
-
-const rl = readline.createInterface({ input, output });
-const a = await rl.question("Masukkan tanggal : \n");
-getDate(a);
-rl.close();
+(async function () {
+  try {
+    const rl = readline.createInterface({ input, output });
+    const a = await rl.question("Masukkan tanggal : \n");
+    getDate(a);
+    rl.close();
+  } catch (error) {
+    console.log("error");
+  }
+});
